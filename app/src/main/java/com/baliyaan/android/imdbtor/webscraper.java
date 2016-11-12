1338384be98a62b5f2040ce70f0c882561c759ec
Pulkit Singh <pulkitsingh01@gmail.com>
2016-11-12 14:44:59 +0530

    local file parsed

--- app/src/main/java/com/baliyaan/android/imdbtor/webscraper.java
+++ app/src/main/java/com/baliyaan/android/imdbtor/webscraper.java
@@ -4,8 +4,12 @@ import android.util.Log;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Pulkit Singh on 10/9/2016.
@@ -19,14 +23,34 @@ public class webscraper {
     }
 
     // "#question .post-text p"
-    public static String Select(Document iDocument,String iSelector)
+    public static ArrayList<String> Select(Document iDocument,String iSelector)
     {
-        String selection = iDocument.select(iSelector).first().text();
+        ArrayList<String> selection = new ArrayList<String>();
+        Elements elements = iDocument.select(iSelector);
+        assert elements!=null;
+        for (int i = 0; i < elements.size() ; i++) {
+            Element element = elements.get(i);
+            String elemText = element.text();
+            selection.add(elemText);
+        }
         return selection;
     }
 
     public static void main(String[] args) {
-        Log.d(TAG, "main: d");
+        try {
+            //Document document = GetDocument("https://www.imdb.com/user/ur27429396/watchlist?ref_=wt_nv_wl_all_0");
+            Document document = Jsoup.parse(new File("D:/Repo/IMDBTor/imdblist.html"),"ISO-8859-1");
+            ArrayList<String> movies = Select(document,"#main > div > span > #center-1-react > div > div:nth-child(3) > div.lister-list.mode-detail > #page-1 > div > div > div.lister-item-content > h3 > a");
+            //ArrayList<String> movies = Select(document,"#main > div > span > #center-1-react > div:nth-child(3)");
+            System.out.println("Number of elements: " + movies.size());
+            if(movies.size()>0) {
+                System.out.println("webscraper.main: " + movies.get(0));
+            }
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
