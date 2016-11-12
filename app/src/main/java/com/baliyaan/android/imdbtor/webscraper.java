4d85ab48cf60d7855cf28a3bb7dfa9a413926bcc
Pulkit Singh <pulkitsingh01@gmail.com>
2016-11-12 15:08:32 +0530

    retrieved magnet element

--- app/src/main/java/com/baliyaan/android/imdbtor/webscraper.java
+++ app/src/main/java/com/baliyaan/android/imdbtor/webscraper.java
@@ -1,14 +1,13 @@
 package com.baliyaan.android.imdbtor;
 
-import android.util.Log;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 /**
@@ -18,17 +17,18 @@ import java.util.ArrayList;
 public class webscraper {
     // "http://stackoverflow.com/questions/2971155"
     public static Document GetDocument(String iURL) throws IOException {
-        Document document = Jsoup.connect(iURL).get();
+        Document document = Jsoup.connect(iURL)
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+                .get();
         return document;
     }
 
     // "#question .post-text p"
-    public static ArrayList<String> Select(Document iDocument,String iSelector)
-    {
+    public static ArrayList<String> Select(Document iDocument, String iSelector) {
         ArrayList<String> selection = new ArrayList<String>();
         Elements elements = iDocument.select(iSelector);
-        assert elements!=null;
-        for (int i = 0; i < elements.size() ; i++) {
+        assert elements != null;
+        for (int i = 0; i < elements.size(); i++) {
             Element element = elements.get(i);
             String elemText = element.text();
             selection.add(elemText);
@@ -37,20 +37,42 @@ public class webscraper {
     }
 
     public static void main(String[] args) {
-        try {
-            //Document document = GetDocument("https://www.imdb.com/user/ur27429396/watchlist?ref_=wt_nv_wl_all_0");
-            Document document = Jsoup.parse(new File("D:/Repo/IMDBTor/imdblist.html"),"ISO-8859-1");
-            ArrayList<String> movies = Select(document,"#main > div > span > #center-1-react > div > div:nth-child(3) > div.lister-list.mode-detail > #page-1 > div > div > div.lister-item-content > h3 > a");
-            //ArrayList<String> movies = Select(document,"#main > div > span > #center-1-react > div:nth-child(3)");
-            System.out.println("Number of elements: " + movies.size());
-            if(movies.size()>0) {
-                System.out.println("webscraper.main: " + movies.get(0));
-            }
 
+        ArrayList<String> torrents = GetTorrents("The Martian");
+        System.out.println("Number of elements: " + torrents.size());
+        if (torrents.size() > 0) {
+            System.out.println("webscraper.main: " + torrents.get(0));
         }
-        catch (IOException e) {
+    }
+
+    private static ArrayList<String> GetTorrents(String q) {
+        String query = null;
+        try {
+            query = URLEncoder.encode(q.toLowerCase(), "UTF-8").replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+        String url = "https://thepiratebay.org/search/" + query + "/0/99/0";
+        String selector = "#searchResult > tbody > tr:nth-child(1) > td:nth-child(2) > a:nth-child(2)";
+        ArrayList<String> torrents = new ArrayList<>();
+        try {
+            Document document = GetDocument(url);
+            torrents = Select(document, selector);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return torrents;
+    }
+
+    private static ArrayList<String> GetListOfMovies() {
+        Document document = null;
+        try {
+            document = GetDocument("https://www.imdb.com/user/ur27429396/watchlist?ref_=wt_nv_wl_all_0");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> movies = Select(document, "#main > div > span > #center-1-react > div > div:nth-child(3) > div.lister-list.mode-detail > #page-1 > div > div > div.lister-item-content > h3 > a");
+        return movies;
     }
 
 }
