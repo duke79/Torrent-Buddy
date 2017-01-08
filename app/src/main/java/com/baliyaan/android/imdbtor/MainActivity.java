3a7fec8f0e8f4afe26f35564cd1bbf4220fa5d9f
Pulkit Singh <pulkitsingh01@gmail.com>
2017-01-08 11:40:21 +0530

    Menu Search

--- app/src/main/java/com/baliyaan/android/imdbtor/MainActivity.java
+++ app/src/main/java/com/baliyaan/android/imdbtor/MainActivity.java
@@ -6,12 +6,33 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener,SearchResultsFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, SearchResultsFragment.OnFragmentInteractionListener {
     public Context mContext;
     SearchView mSearchView = null;
     SearchResultsFragment mSearchResultsFragment = null;
     LoginFragment mLoginFragment = null;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                setupSearchResultsFragment("");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +45,23 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
     }
 
     private void setupSearchResultsFragment(String query) {
-        if(mSearchResultsFragment==null) {
+        if (mSearchResultsFragment == null) {
             mSearchResultsFragment = new SearchResultsFragment();
         }
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if(mLoginFragment!=null)
+        if (mLoginFragment != null)
             transaction.remove(mLoginFragment);
-        transaction.add(R.id.activity_main,mSearchResultsFragment).commit();
+        transaction.add(R.id.activity_main, mSearchResultsFragment).commit();
         mSearchResultsFragment.Initiate(query);
     }
 
     private void setupLoginFragment() {
-        if(mLoginFragment==null)
+        if (mLoginFragment == null)
             mLoginFragment = new LoginFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if(mSearchResultsFragment!=null)
+        if (mSearchResultsFragment != null)
             transaction.remove(mSearchResultsFragment);
-        transaction.add(R.id.activity_main,mLoginFragment).commit();
+        transaction.add(R.id.activity_main, mLoginFragment).commit();
     }
 
     private void setupSearchView() {
